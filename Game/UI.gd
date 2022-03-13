ba24ecb3403b912882c2d5b605e7bbd4edbb3344
JJC3PO <42073438+JJC3PO@users.noreply.github.com>
2022-03-13 12:17:37 -0700

    Add files via upload

---
+++ Game/UI.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+signal startGame
+	
+func _on_MessageTimer_timeout():
+	get_node("Message").hide()
+
+func showMessage(text):
+	get_node("Message").text = text
+	get_node("Message").show()
+	get_node("MessageTimer").start()
+	
+func showGameOver():
+	showMessage("Dead")
+	yield(get_node("MessageTimer"), "timeout")
+	get_node("Message").text = "Why am I here?"
+	get_node("Message").show()
+	get_node("Start").show()
+	
+func updateScore(score):
+	get_node("Score").text = str(score)
+		
+
+	
+	
+
+func _on_Start_pressed():
+	get_node("Start").hide()
+	emit_signal("startGame")
